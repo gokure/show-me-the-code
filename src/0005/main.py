d354ae315478c5af16429c325218e98a20cf137e
Gang Wu <gokure@gmail.com>
2014-12-06 11:41:21 +0800

    resize images to compatible with iPhone 5

---
+++ src/0005/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# pip install PIL
+
+from os import path
+from glob import glob
+from PIL import Image
+
+def image_resize(filename):
+    max_height, max_width = 1136, 640 # iPhone 5' screen is
+    try:
+        with Image.open(filename) as img:
+            raw_width, raw_height = img.size
+            width_ratio = float(max_width) / raw_width if raw_width > max_width else 1
+            height_ratio = float(max_height) / raw_height if raw_height > max_height else 1
+            ratio = width_ratio if height_ratio > width_ratio else height_ratio
+            new_width = int(raw_width*ratio)
+            new_height = int(raw_height*ratio)
+            img.resize((new_width, new_height), Image.ANTIALIAS).save(filename, quality=95)
+    except IOError:
+        print "{0} cannot open, maybe it's not picture.".format(filename)
+
+if __name__ == '__main__':
+    for filename in glob(path.abspath(path.join(path.abspath(__file__), '..', 'images', '*'))):
+        image_resize(filename)
