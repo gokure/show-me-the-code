a3b9e4d5a9975eee6bb68399584def9106ddadf6
Gang Wu <gokure@gmail.com>
2014-12-07 19:26:53 +0800

    Fix homework 0004

--- src/0004/main.py
+++ src/0004/main.py
@@ -3,9 +3,8 @@
 #
 
 from os import path
-from collections import Counter
 
 if __name__ == '__main__':
     with open(path.join(path.abspath(__file__), '..', 'example.txt')) as f:
         words = f.read().strip().split()
-        print Counter(words).items()
+        print(len(words))
